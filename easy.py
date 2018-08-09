eaafce0dbdb7f35781566dbc58409e22f6e5887e
armor <flakoh@yandex.ru>
2018-08-09 17:39:20 +0300

    Выполнил домашнее задание на легком уровне

---
+++ easy.py
@@ -0,0 +1,22 @@
+
+# #Задача №1
+# number1 = input('Введите 1:')
+# number2 = input('Введите 2:')
+# number3 = input('Введите 3:')
+# number4 = input('Введите 4:')
+
+
+#Задача №2
+# number1 = int(input('Введите число:'))
+# print(number1 + 2)
+
+
+#Задача №3
+
+# age = int(input(' Введите возраст'))
+# age_control = 18
+#
+# if age >= age_control:
+#     print('Доступ разрешен')
+# else:
+#     print('Извините, пользование данным ресурсом только с 18 лет')

37bda798805b616c4709252410015aed0171a0ec
armor <flakoh@yandex.ru>
2018-08-09 22:31:50 +0300

    Коррекция кода

--- easy.py
+++ easy.py
@@ -1,19 +1,21 @@
 
-# #Задача №1
-# number1 = input('Введите 1:')
-# number2 = input('Введите 2:')
-# number3 = input('Введите 3:')
-# number4 = input('Введите 4:')
+#Задача №1
+# number_1 = input('Введите 1:')
+# number_2 = input('Введите 2:')
+# number_3 = input('Введите 3:')
+# number_4 = input('Введите 4:')
 
 
 #Задача №2
-# number1 = int(input('Введите число:'))
-# print(number1 + 2)
+
+# number_2 = 2
+# number_1 = int(input('Введите число:'))
+# print(number_1 + number_2)
 
 
 #Задача №3
-
-# age = int(input(' Введите возраст'))
+#
+# age = int(input(' Введите возраст:'))
 # age_control = 18
 #
 # if age >= age_control:
